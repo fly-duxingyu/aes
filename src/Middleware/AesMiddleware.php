4f163d3ba2093c4c8a5c95c061eddf722e1ef62d
duxingyu <530606930@qq.com>
2021-10-25 14:02:43 +0800

    添加重复点击验证需要传递unique_key

--- src/Middleware/AesMiddleware.php
+++ src/Middleware/AesMiddleware.php
@@ -3,9 +3,11 @@
 namespace Duxingyu\Aes\Middleware;
 
 use Closure;
+use Composer\Config;
 use Duxingyu\Aes\Php\Aes;
 use ErrorException;
 use Illuminate\Http\Request;
+use Cache;
 
 class AesMiddleware
 {
@@ -19,9 +21,30 @@ class AesMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        //获取参数
         $params = $request->input('params');
-        $data = Aes::init()->decrypt($params);
-        $request->merge(json_decode($data, true));
+
+        //解密参数
+        $dataJson = Aes::init()->decrypt($params);
+
+        //json转数组
+        $data = $dataJson ? json_decode($dataJson, true) : [];
+        //获取唯一的key或者token
+        $unique_key = $data['unique_key'] . '$_repeat_click_time';
+
+        //查看是否存在唯一key或者token
+        $value = Cache::get($unique_key);
+        //已存在同样的key和数据
+        if (!empty($value) && $value == $dataJson) {
+            return response(['message' => '请勿重复点击', 'code' => 401]);
+        }
+        //不存在重复点击 上锁 默认3秒自动解除
+        $time = Config('duxingyuConfig.repeat_click_time') ?: 3;
+        Cache::set($unique_key, $dataJson, $time);
+
+        //合并post数据
+        unset($data['unique_key']);
+        $request->merge($data);
         return $next($request);
     }
 }
